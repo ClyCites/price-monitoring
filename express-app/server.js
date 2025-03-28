ce01b03293bc5d1a7ce00085b8736d4185181a45
Daniel Ntege <danientege785@gmail.com>
2025-03-28 14:37:40 +0300

    commit

--- express-app/server.js
+++ express-app/server.js
@@ -4,6 +4,7 @@ import connectDB from './src/config/db.js';
 import authRoutes from './src/routes/authRoutes.js';
 import priceRoutes from './src/routes/priceRoutes.js';
 import marketRoutes from './src/routes/marketRoutes.js';
+import productRoutes from './src/routes/productRoutes.js';
 import { errorHandler, notFound } from './src/middleware/errorMiddleware.js';
 import cors from 'cors';
 
@@ -17,6 +18,7 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/prices', priceRoutes);
 app.use('/api/markets', marketRoutes);
+app.use('/api/products', productRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
